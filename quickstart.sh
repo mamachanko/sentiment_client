ce457795c64d2b73074e6740ea121afb393b7684
mamachanko <max@rootswiseyouths.com>
2015-04-12 11:02:54 +0200

    add comments to quickstart

--- quickstart.sh
+++ quickstart.sh
@@ -1,6 +1,14 @@
+# clone sentiment_client
 git clone git@github.com:mamachanko/sentiment_client.git
+
+# move into the directory
 cd sentiment_client
+
+# create a virtualenv and activate it
 virtualenv venv
-source venv/bin/activate
+
+# install lymph
 pip install git+ssh://git@github.com/deliveryhero/lymph@master#egg=lymph 
+
+# run the service
 PYTHONPATH=. lymph node
